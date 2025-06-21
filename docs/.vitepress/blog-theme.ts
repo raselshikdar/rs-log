a5516781c514730f8daaa67eed21ec3522812b80
Rasel Shikdar <imrasel597@gmail.com>
2025-06-21 21:22:38 +0600

    Update blog-theme.ts

--- docs/.vitepress/blog-theme.ts
+++ docs/.vitepress/blog-theme.ts
@@ -1,29 +1,72 @@
 // Theme-specific configuration
 import { getThemeConfig } from '@sugarat/theme/node'
 
-export const blogTheme = getThemeConfig({
-  themeColor: 'el-blue',
-  author: 'Rasel Shikdar',
+// Enable RSS support (RSS configuration)
+// import type { Theme } from '@sugarat/theme'
 
+// const baseUrl = 'https://sugarat.top'
+// const RSS: Theme.RSSOptions = {
+//   title: 'Sugar in the Porridge',
+//   baseUrl,
+//   copyright: 'Copyright (c) 2018-present, Sugar in the Porridge',
+//   description: 'The power to change the world is at your fingertips (Front-end tech sharing)',
+//   language: 'zh-cn',
+//   image: 'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
+//   favicon: 'https://sugarat.top/favicon.ico',
+// }
+
+// All configuration options, see docs: https://theme.sugarat.top/
+const blogTheme = getThemeConfig({
+  // Enable RSS support
+  // RSS,
+
+  // Search
+  // Pagefind offline full-text search is enabled by default (set to false if using another solution)
+  // search: false,
+
+  // Markdown chart support (adds some build time)
+  // mermaid: true
+
+  // Footer
   footer: {
-    copyright: 'MIT Licensed | RsLog'
+    // The message field supports HTML content. Use an array to configure multiple lines.
+    // message: 'The content and icons below can all be customized (this message too)',
+    copyright: 'MIT Licensed | RsLog',
+    // icpRecord: {
+    //   name: 'Sichuan ICP No. 19011724',
+    //   link: 'https://beian.miit.gov.cn/'
+    // },
+    // securityRecord: {
+    //   name: 'Public Security Registration xxxxx',
+    //   link: 'https://www.beian.gov.cn/portal/index.do'
+    // },
   },
 
+  // Theme color customization
+  themeColor: 'el-blue',
+
+  // Default author of articles
+  author: 'Rasel Shikdar',
+
+  // Friend links
   friend: [
     {
       nickname: 'RsLog',
       des: 'A journal of ideas, by Rasel Shikdar',
-      avatar: 'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
+      avatar:
+        'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
       url: 'https://rslog.vercel.app',
     },
     {
       nickname: 'Vitepress',
       des: 'Vite & Vue Powered Static Site Generator',
-      avatar: 'https://vitepress.dev/vitepress-logo-large.webp',
+      avatar:
+        'https://vitepress.dev/vitepress-logo-large.webp',
       url: 'https://vitepress.dev/',
     },
   ],
 
+  // Announcement
   popover: {
     title: 'Announcement',
     body: [
@@ -32,44 +75,31 @@ export const blogTheme = getThemeConfig({
         type: 'image',
         src: 'https://img.cdn.sugarat.top/mdImg/MTYxNTAxODc2NTIxMA==615018765210~fmt.webp'
       },
-      { type: 'text', content: 'Feel free to join the group & DM for discussions' },
-      { type: 'text', content: 'Group QR codes are shown at the beginning/end of articles', style: 'padding-top:0' },
-      { type: 'button', content: 'Author Blog', link: '/' },
+      {
+        type: 'text',
+        content: 'Feel free to join the group & DM for discussions'
+      },
+      {
+        type: 'text',
+        content: 'Group QR codes are shown at the beginning/end of articles',
+        style: 'padding-top:0'
+      },
+      {
+        type: 'button',
+        content: 'Author Blog',
+        link: '/'
+      },
       {
         type: 'button',
         content: 'Join group discussion',
-        props: { type: 'success' },
-        link: 'https://t.me/rasel597'
+        props: {
+          type: 'success'
+        },
+        link: 'https://t.me/rasel597',
       }
     ],
     duration: 0
   },
-
-  // i18n: full English interface
-  i18n: {
-    friendLinks: 'Friend Links',
-    tag: 'Tags',
-    category: 'Category',
-    categories: 'Categories',
-    publishTime: 'Published',
-    lastUpdated: 'Last Updated',
-    wordCount: 'Word Count',
-    readingTime: 'Reading Time',
-    views: 'Views',
-    noRelatedPosts: 'No related posts found',
-    returnToTop: 'Back to Top',
-    more: 'More',
-    toc: 'Table of Contents',
-    recommended: 'Featured Articles',
-    articleList: 'Blog Posts',
-    articleListTips: 'All articles are listed below',
-    monthlyUpdates: 'Updates this Month',
-    weeklyUpdates: 'Updates this Week',
-    latestArticles: 'Latest Articles',
-    latestProjects: 'Latest Projects',
-    all: 'All',
-    loadMore: 'Load More',
-    loading: 'Loading...',
-    empty: 'No content available'
-  }
 })
+
+export { blogTheme }

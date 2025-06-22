b02d53dd7966a88b5ec2cc5722b8633cbe921f61
Rasel Shikdar <imrasel597@gmail.com>
2025-06-23 02:04:29 +0600

    Update blog-theme.ts

--- docs/.vitepress/blog-theme.ts
+++ docs/.vitepress/blog-theme.ts
@@ -1,54 +1,16 @@
-// Theme-specific configuration
 import { getThemeConfig } from '@sugarat/theme/node'
 
-// Enable RSS support (RSS configuration)
-// import type { Theme } from '@sugarat/theme'
-
-// const baseUrl = 'https://sugarat.top'
-// const RSS: Theme.RSSOptions = {
-//   title: 'Sugar in the Porridge',
-//   baseUrl,
-//   copyright: 'Copyright (c) 2018-present, Sugar in the Porridge',
-//   description: 'The power to change the world is at your fingertips (Front-end tech sharing)',
-//   language: 'zh-cn',
-//   image: 'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
-//   favicon: 'https://sugarat.top/favicon.ico',
-// }
-
-// All configuration options, see docs: https://theme.sugarat.top/
 const blogTheme = getThemeConfig({
-  // Enable RSS support
-  // RSS,
-
-  // Search
-  // Pagefind offline full-text search is enabled by default (set to false if using another solution)
-  // search: false,
-
-  // Markdown chart support (adds some build time)
-  // mermaid: true
-
-  // Footer
   footer: {
-    // The message field supports HTML content. Use an array to configure multiple lines.
-    // message: 'The content and icons below can all be customized (this message too)',
     copyright: 'MIT Licensed | RsLog',
-    // icpRecord: {
-    //   name: 'Sichuan ICP No. 19011724',
-    //   link: 'https://beian.miit.gov.cn/'
-    // },
-    // securityRecord: {
-    //   name: 'Public Security Registration xxxxx',
-    //   link: 'https://www.beian.gov.cn/portal/index.do'
-    // },
   },
 
-  // Theme color customization
   themeColor: 'el-blue',
 
   // Default author of articles
   author: 'Rasel Shikdar',
 
-  // Friend links
+  // Friend links - only one friend link kept
   friend: [
     {
       nickname: 'RsLog',
@@ -57,16 +19,29 @@ const blogTheme = getThemeConfig({
         'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
       url: 'https://rslog.vercel.app',
     },
-    {
-      nickname: 'Vitepress',
-      des: 'Vite & Vue Powered Static Site Generator',
-      avatar:
-        'https://vitepress.dev/vitepress-logo-large.webp',
-      url: 'https://vitepress.dev/',
-    },
   ],
 
-  // Announcement
+  // Comment system configuration using Giscus
+  comment: {
+    type: 'giscus',
+    options: {
+      repo: 'raselshikdar/rs-log',
+      repoId: 'R_kgDOO_chsA',
+      category: 'General',
+      categoryId: 'DIC_kwDOO_chsM4Cr2HD',
+      inputPosition: 'top',
+    },
+  },
+
+  // Appreciation button configuration
+  buttonAfterArticle: {
+    openTitle: 'Support with a "Tip"',
+    closeTitle: 'Maybe next time',
+    content: '<img src="https://img.cdn.sugarat.top/mdImg/MTY0Nzc1NTYyOTE5Mw==647755629193">',
+    icon: 'wechatPay',
+  },
+
+  // Announcement popover configuration
   popover: {
     title: 'Announcement',
     body: [

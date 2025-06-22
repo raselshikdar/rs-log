954b836332b2476bb65af7b1775dee987e8285dd
Rasel Shikdar <imrasel597@gmail.com>
2025-06-23 00:30:36 +0600

    Update CommentWaline.vue

--- docs/.vitepress/theme/components/CommentWaline.vue
+++ docs/.vitepress/theme/components/CommentWaline.vue
@@ -1,34 +1,26 @@
 <script setup>
-import { onMounted } from 'vue'
 import { init } from '@waline/client'
-import '@waline/client/dist/waline.css'
-
-const walineOptions = {
-  el: '#waline-comment',
-  serverURL: 'https://raselverse-waline.vercel.app', // your actual server URL
-  lang: 'en',
-  reaction: true,
-  emoji: [
-    'https://unpkg.com/@waline/emojis@1.1.0/weibo',
-    'https://unpkg.com/@waline/emojis@1.1.0/bilibili',
-    'https://unpkg.com/@waline/emojis@1.1.0/tieba'
-  ],
-  requiredMeta: ['name', 'email'],
-  login: 'enable',
-  pageview: true
-}
+import '@waline/client/style'
+import { onMounted } from 'vue'
 
 onMounted(() => {
-  init(walineOptions)
+  init({
+    el: '#waline',
+    serverURL: 'https://raselverse-waline.vercel.app', // your actual server
+    lang: 'en',
+    reaction: true,
+    emoji: [
+      'https://unpkg.com/@waline/emojis@1.1.0/weibo',
+      'https://unpkg.com/@waline/emojis@1.1.0/bilibili',
+      'https://unpkg.com/@waline/emojis@1.1.0/tieba'
+    ],
+    requiredMeta: ['name', 'email'],
+    login: 'enable',
+    pageview: true
+  })
 })
 </script>
 
 <template>
-  <div id="waline-comment" class="waline-wrapper" />
+  <div id="waline" style="margin-top: 2rem;"></div>
 </template>
-
-<style scoped>
-.waline-wrapper {
-  margin-top: 2rem;
-}
-</style>

c5fbcbc012b7127236b10b4b3353eb8102f7583f
Rasel Shikdar <imrasel597@gmail.com>
2025-06-23 14:03:45 +0600

    Update blog.ts

--- sugar-blog/packages/theme/src/composables/config/blog.ts
+++ sugar-blog/packages/theme/src/composables/config/blog.ts
@@ -414,20 +414,20 @@ export function useFormatShowDate() {
     return function formatShowDate(date: any) {
       const utcDate = new Date(date)
 
-      const formatter = new Intl.DateTimeFormat('en-GB', {
+      const formatter = new Intl.DateTimeFormat('en-US', {
         timeZone: 'Asia/Dhaka',
         year: 'numeric',
         month: '2-digit',
         day: '2-digit',
         hour: '2-digit',
         minute: '2-digit',
-        hour12: false
+        hour12: true
       })
 
       const parts = formatter.formatToParts(utcDate)
-      const get = (type: string) => parts.find(p => p.type === type)?.value.padStart(2, '0')
+      const get = (type: string) => parts.find(p => p.type === type)?.value
 
-      return `${get('year')}-${get('month')}-${get('day')} ${get('hour')}:${get('minute')}`
+      return `${get('year')}-${get('month')}-${get('day')} ${get('hour')}:${get('minute')} ${get('dayPeriod')}`
     }
   })
 }

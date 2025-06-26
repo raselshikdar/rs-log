eafa421eb71e15b12e0df381423878d4d5d3cfb6
Rasel Shikdar <imrasel597@gmail.com>
2025-06-26 12:16:12 +0600

    Update blog-theme.ts

--- docs/.vitepress/blog-theme.ts
+++ docs/.vitepress/blog-theme.ts
@@ -2,8 +2,8 @@ import { getThemeConfig } from '@sugarat/theme/node'
 
 const blogTheme = getThemeConfig({
   footer: {
-  copyright: 'MIT Licensed | RsLog',
-message: '© 2025 <a href="https://rasel.is-a.dev" target="_blank" rel="noopener noreferrer">Rasel Shikdar</a> | All rights reserved',
+  message: '[About](/about.html) | [Contact](/contact.html) | [Privacy](/privacy.html) | [T&C](/terms.html)',
+  copyright: '2025 [Rasel Shikdar](https://rasel.is-a.dev) | All rights reserved',
 },
 
   themeColor: 'el-blue',

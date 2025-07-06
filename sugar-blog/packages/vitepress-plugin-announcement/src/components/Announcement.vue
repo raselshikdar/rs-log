49e119f01c35566a9dbb5d1efc380e79131afbc7
Rasel Shikdar <imrasel597@gmail.com>
2025-07-07 00:29:40 +0600

    Update Announcement.vue

--- sugar-blog/packages/vitepress-plugin-announcement/src/components/Announcement.vue
+++ sugar-blog/packages/vitepress-plugin-announcement/src/components/Announcement.vue
@@ -12,14 +12,14 @@ import { inBrowser, parseStringStyle, useDebounceFn, useWindowSize } from './uti
 const { localeIndex } = useData()
 const popoverProps = computed<AnnouncementOptions>(() => ({ ...announcementOptions, ...announcementOptions?.locales?.[localeIndex.value] }))
 
-// ======== Modified Section ========
+// ========== MODIFIED SECTION START ==========
 const storageKey = computed(() => `vitepress-plugin-announcement-${localeIndex.value}`)
 const closeFlag = computed(() => `${storageKey.value}-close`)
-const firstVisitKey = computed(() => `${storageKey.value}-first-visit`)
+const firstVisitKey = computed(() => `announcement-first-visit-${localeIndex.value}`)
 
-// Initialize as false - won't show automatically
+// Initialize as hidden
 const show = ref(false)
-// ======== End Modified Section ========
+// ========== MODIFIED SECTION END ==========
 
 const bodyContent = computed(() => {
   return popoverProps.value?.body || []
@@ -43,43 +43,33 @@ watch(popoverProps, () => {
     return
   }
 
-  // 取旧值
+  // ========== MODIFIED SECTION START ==========
+  // First visit check
+  if (!localStorage.getItem(firstVisitKey.value)) {
+    localStorage.setItem(firstVisitKey.value, 'true')
+    
+    // Only show if duration is >= 0 (original behavior)
+    if ((popoverProps.value?.duration ?? 0) >= 0) {
+      show.value = true
+      
+      // Auto-hide after duration if specified
+      if (popoverProps.value?.duration) {
+        setTimeout(() => {
+          show.value = false
+        }, popoverProps.value.duration)
+      }
+    }
+  }
+  // ========== MODIFIED SECTION END ==========
+
+  // Original storage logic (kept but won't trigger show)
   const oldValue = localStorage.getItem(storageKey.value)
   const newValue = JSON.stringify(popoverProps.value)
   localStorage.setItem(storageKey.value, newValue)
 
-  // ======== Modified Section ========
-  // First visit logic
-  if (!localStorage.getItem(firstVisitKey.value)) {
-    localStorage.setItem(firstVisitKey.value, 'true')
-    // Only show automatically if duration is >= 0 (original behavior)
-    if ((popoverProps.value?.duration ?? 0) >= 0) {
-      show.value = true
-      if (popoverProps.value?.duration) {
-        setTimeout(() => {
-          show.value = false
-        }, popoverProps.value?.duration)
-      }
-    }
-    return
-  }
-  // ======== End Modified Section ========
-
   // 移动端最小化
   if (width.value < 768 && popoverProps.value?.mobileMinify) {
     show.value = false
-    return
-  }
-
-  // Original logic for non-first visits (kept but won't trigger show automatically)
-  if (oldValue !== newValue && popoverProps.value?.duration === -1) {
-    // 当做新值处理
-    localStorage.removeItem(closeFlag.value)
-    return
-  }
-
-  if (oldValue === newValue && popoverProps.value?.duration === -1 && !localStorage.getItem(closeFlag.value)) {
-    // Don't automatically show on subsequent visits
     return
   }
 }, { immediate: true })
@@ -180,12 +170,18 @@ const showReopen = computed(() => {
     <div class="header">
       <div class="title-wrapper">
         <AnnouncementIcon size="20px" :icon="popoverProps.icon">
-          <svg t="1716085184855" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4274" width="200" height="200"><path d="M660.48 872.448q6.144 0-3.584 15.36t-29.696 33.792-47.104 33.792-57.856 15.36q-27.648 0-53.248-15.36t-45.056-33.792-29.696-33.792-6.144-15.36l272.384 0zM914.432 785.408q7.168 9.216 6.656 17.92t-4.608 14.848-10.24 9.728-12.288 3.584l-747.52 0q-14.336 0-20.992-11.776t4.608-29.184q17.408-30.72 40.96-68.608t44.544-81.408 36.352-92.16 15.36-101.888q0-51.2 14.336-92.16t37.376-71.68 53.248-52.224 62.976-32.768q-16.384-26.624-16.384-55.296 0-41.984 28.672-70.656t70.656-28.672 70.656 28.672 28.672 70.656q0 14.336-4.096 28.16t-11.264 25.088q34.816 11.264 66.048 32.768t54.272 53.248 36.864 72.704 13.824 91.136q0 51.2 15.36 100.864t36.864 94.208 45.568 81.408 43.52 63.488zM478.208 142.336q0 16.384 11.264 28.16t27.648 11.776l2.048 0q16.384-1.024 27.648-12.288t11.264-27.648q0-17.408-11.264-28.672t-28.672-11.264-28.672 11.264-11.264 28.672z" p-id="4275" /></svg>
+          <!-- BELL ICON SVG (EXACTLY AS IN YOUR ORIGINAL) -->
+          <svg t="1716085184855" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4274" width="200" height="200">
+            <path d="M660.48 872.448q6.144 0-3.584 15.36t-29.696 33.792-47.104 33.792-57.856 15.36q-27.648 0-53.248-15.36t-45.056-33.792-29.696-33.792-6.144-15.36l272.384 0zM914.432 785.408q7.168 9.216 6.656 17.92t-4.608 14.848-10.24 9.728-12.288 3.584l-747.52 0q-14.336 0-20.992-11.776t4.608-29.184q17.408-30.72 40.96-68.608t44.544-81.408 36.352-92.16 15.36-101.888q0-51.2 14.336-92.16t37.376-71.68 53.248-52.224 62.976-32.768q-16.384-26.624-16.384-55.296 0-41.984 28.672-70.656t70.656-28.672 70.656 28.672 28.672 70.656q0 14.336-4.096 28.16t-11.264 25.088q34.816 11.264 66.048 32.768t54.272 53.248 36.864 72.704 13.824 91.136q0 51.2 15.36 100.864t36.864 94.208 45.568 81.408 43.52 63.488zM478.208 142.336q0 16.384 11.264 28.16t27.648 11.776l2.048 0q16.384-1.024 27.648-12.288t11.264-27.648q0-17.408-11.264-28.672t-28.672-11.264-28.672 11.264-11.264 28.672z" p-id="4275" />
+          </svg>
         </AnnouncementIcon>
         <span class="title">{{ popoverProps?.title }}</span>
       </div>
       <AnnouncementIcon class="close-icon" size="20px" :icon="popoverProps?.closeIcon" @click="handleClose">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="currentColor" d="M512 64a448 448 0 1 1 0 896 448 448 0 0 1 0-896m0 393.664L407.936 353.6a38.4 38.4 0 1 0-54.336 54.336L457.664 512 353.6 616.064a38.4 38.4 0 1 0 54.336 54.336L512 566.336 616.064 670.4a38.4 38.4 0 1 0 54.336-54.336L566.336 512 670.4 407.936a38.4 38.4 0 1 0-54.336-54.336z" /></svg>
+        <!-- CLOSE ICON SVG (EXACTLY AS IN YOUR ORIGINAL) -->
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024">
+          <path fill="currentColor" d="M512 64a448 448 0 1 1 0 896 448 448 0 0 1 0-896m0 393.664L407.936 353.6a38.4 38.4 0 1 0-54.336 54.336L457.664 512 353.6 616.064a38.4 38.4 0 1 0 54.336 54.336L512 566.336 616.064 670.4a38.4 38.4 0 1 0 54.336-54.336L566.336 512 670.4 407.936a38.4 38.4 0 1 0-54.336-54.336z" />
+        </svg>
       </AnnouncementIcon>
     </div>
     <div v-if="bodyContent.length" class="body content">
@@ -206,7 +202,10 @@ const showReopen = computed(() => {
     @click="show = true"
   >
     <AnnouncementIcon :icon="popoverProps?.icon">
-      <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4274" width="200" height="200"><path d="M660.48 872.448q6.144 0-3.584 15.36t-29.696 33.792-47.104 33.792-57.856 15.36q-27.648 0-53.248-15.36t-45.056-33.792-29.696-33.792-6.144-15.36l272.384 0zM914.432 785.408q7.168 9.216 6.656 17.92t-4.608 14.848-10.24 9.728-12.288 3.584l-747.52 0q-14.336 0-20.992-11.776t4.608-29.184q17.408-30.72 40.96-68.608t44.544-81.408 36.352-92.16 15.36-101.888q0-51.2 14.336-92.16t37.376-71.68 53.248-52.224 62.976-32.768q-16.384-26.624-16.384-55.296 0-41.984 28.672-70.656t70.656-28.672 70.656 28.672 28.672 70.656q0 14.336-4.096 28.16t-11.264 25.088q34.816 11.264 66.048 32.768t54.272 53.248 36.864 72.704 13.824 91.136q0 51.2 15.36 100.864t36.864 94.208 45.568 81.408 43.52 63.488zM478.208 142.336q0 16.384 11.264 28.16t27.648 11.776l2.048 0q16.384-1.024 27.648-12.288t11.264-27.648q0-17.408-11.264-28.672t-28.672-11.264-28.672 11.264-11.264 28.672z" p-id="4275" /></svg>
+      <!-- REOPEN ICON SVG (EXACTLY AS IN YOUR ORIGINAL) -->
+      <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4274" width="200" height="200">
+        <path d="M660.48 872.448q6.144 0-3.584 15.36t-29.696 33.792-47.104 33.792-57.856 15.36q-27.648 0-53.248-15.36t-45.056-33.792-29.696-33.792-6.144-15.36l272.384 0zM914.432 785.408q7.168 9.216 6.656 17.92t-4.608 14.848-10.24 9.728-12.288 3.584l-747.52 0q-14.336 0-20.992-11.776t4.608-29.184q17.408-30.72 40.96-68.608t44.544-81.408 36.352-92.16 15.36-101.888q0-51.2 14.336-92.16t37.376-71.68 53.248-52.224 62.976-32.768q-16.384-26.624-16.384-55.296 0-41.984 28.672-70.656t70.656-28.672 70.656 28.672 28.672 70.656q0 14.336-4.096 28.16t-11.264 25.088q34.816 11.264 66.048 32.768t54.272 53.248 36.864 72.704 13.824 91.136q0 51.2 15.36 100.864t36.864 94.208 45.568 81.408 43.52 63.488zM478.208 142.336q0 16.384 11.264 28.16t27.648 11.776l2.048 0q16.384-1.024 27.648-12.288t11.264-27.648q0-17.408-11.264-28.672t-28.672-11.264-28.672 11.264-11.264 28.672z" p-id="4275" />
+      </svg>
     </AnnouncementIcon>
   </div>
 </template>
